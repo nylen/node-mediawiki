6e37c8e7135f52899ed0687c00937d0a00c0dd7f
James Nylen <jnylen@gmail.com>
2012-10-09 15:33:50 -0400

    Finish implementing wiki-edit-page.js script

--- bin/wiki-edit-page.js
+++ bin/wiki-edit-page.js
@@ -1,7 +1,28 @@
 #!/usr/bin/env node
 
-var wiki = require('../lib/wiki');
+var fs = require('fs'),
+    path = require('path'),
+    pipette = require('pipette'),
+    wiki = require('../lib/wiki');
 
-getPageMarkup('XBMC', function(text) {
-    editPage('XBMC', text, 'Test editing API');
-});
+var title = process.argv[2],
+    fn = process.argv[3];
+
+var edit = function(text) {
+    wiki.editPage(title, text,
+        'Edited with ' + path.basename(process.argv[1]), console.error);
+};
+
+if (fn) {
+    edit(fs.readFileSync(fn));
+} else {
+    var text = '';
+    process.stdin.resume();
+    (new pipette.Sink(process.stdin)).on('data', function(data) {
+        text = data.toString();
+    }).on('error', function(error) {
+        throw new Error('Error reading stdin: ' + error);
+    }).on('end', function() {
+        edit(text);
+    });
+}
