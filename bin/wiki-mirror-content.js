251dbfa2c30ee1768473d7dff5fe6d0f513dbdf7
James Nylen <jnylen@gmail.com>
2013-04-11 22:56:39 -0400

    Tweak wiki-mirror-content.js

--- bin/wiki-mirror-content.js
+++ bin/wiki-mirror-content.js
@@ -1,46 +1,45 @@
 #!/usr/bin/env node
 
-var fs = require('fs'),
+var fs   = require('fs-extra'),
     path = require('path'),
     util = require('util'),
     wiki = require('../lib/wiki');
 
-var dir = process.argv[2];
+fs.jsonfile.spaces = 4;
 
-if (!dir) {
+var mirrorDir = process.argv[2];
+
+if (!mirrorDir) {
     throw new Error(util.format(
-        'No mirror directory given.  Usage: %s mirror-directory', process.argv[1]));
+        'No mirror directory given.  Usage: %s mirror-directory',
+        process.argv[1]));
 }
 
-try {
-    var stat = fs.statSync(dir);
-} catch (ex) { }
+fs.mkdirpSync(mirrorDir);
 
-if (!stat || !stat.isDirectory()) {
-    fs.mkdirSync(dir);
-}
-
-var titleToFileMap = {};
-var numTitles = 0;
-var gotAllTitles = false;
-var numPages = 0;
+var titleToFileMap = {},
+    numTitles = 0,
+    gotAllTitles = false,
+    numPages = 0;
 
 wiki.listPages(function(title) {
     var filename = wiki.pageTitleToFilename(title);
     titleToFileMap[title] = filename;
     numTitles++;
     wiki.getPageContent(title, function(data) {
-        filename = path.join(dir, filename);
+        filename = path.join(mirrorDir, filename);
         fs.writeFileSync(filename, data);
         console.log(util.format(
             "Wrote page '%s' to file '%s'", title, filename));
         numPages++;
 
         if (gotAllTitles && numTitles == numPages) {
-            var mapFilename = path.join(dir, 'wiki_pages.json');
-            fs.writeFileSync(mapFilename, JSON.stringify(titleToFileMap, null, 4) + "\n");
-            console.log(util.format(
-                "Wrote page titles and filenames to '%s'", mapFilename));
+            var mapFilename = path.join(mirrorDir, 'wiki_pages.json');
+            fs.writeJSONFile(mapFilename, titleToFileMap, function(err) {
+                if (err) throw err;
+                console.log(util.format(
+                    "Wrote page titles and filenames to '%s'", mapFilename));
+            });
         }
     });
 }, function() {
